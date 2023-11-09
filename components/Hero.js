e653a32f0274f943524495de6520cc24c86ce069
Jecy Jehu <jecyjehu@outlook.com>
2023-11-09 22:09:32 +0100

    deploy

--- components/Hero.js
+++ components/Hero.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import nuts from "@/images/nuts.svg"
 import nuts2 from "@/images/nuts2.png"
 import coffee from "@/images/coffee.png"
+import { Button } from "react-bootstrap";
 
 
 const Hero = () => {
@@ -11,6 +12,11 @@ const Hero = () => {
         <div className="relative z-50 grid md:grid-cols-12 w-full pt-24">
             <div className="col-span-6 pb-7 md:pb-0">
                 <h1 className="text-4xl font-bold">Enjoy your <span className="text-[#FF902A]">coffee</span> before your activity</h1>
+                <p>Boost your productivity and build your mood with a glass of coffee in the morning </p>
+                <div className="space-x-3">
+                  <button className="bg-[#2F2105] text-white rounded-3xl py-2 w-32">Order Now</button>
+                  <button className="bg-transparent text-[#FF902A] border-none py-2 w-32">More Menu</button>
+                </div>
             </div>
             <div className="col-span-6">
                 <Image src={coffee} alt="coffee" priority />
