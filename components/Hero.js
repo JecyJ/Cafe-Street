afbce1d82d8dfd54df6c7b1eccba4549d0a6ac8f
Jecy Jehu <jecyjehu@outlook.com>
2023-11-13 10:48:00 +0100

    deploy

--- components/Hero.js
+++ components/Hero.js
@@ -3,18 +3,21 @@ import nuts from "@/images/nuts.svg"
 import nuts2 from "@/images/nuts2.png"
 import coffee from "@/images/coffee.png"
 import { Button } from "react-bootstrap";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 
 const Hero = () => {
   return (
-    <section className="w-full h-full bg-[#fae5c2]">
-        <Image src={nuts} alt="nuts" priority className="absolute right-0" />
-        <div className="relative z-50 grid md:grid-cols-12 w-full pt-24">
-            <div className="col-span-6 pb-7 md:pb-0">
-                <h1 className="text-4xl font-bold">Enjoy your <span className="text-[#FF902A]">coffee</span> before your activity</h1>
+    <section className="relative w-full h-full bg-[#fae5c2]">
+      <Image src={nuts} alt="nuts" priority className="absolute right-0 top-[220px] md:top-0 w-[250px] md:w-[400px] h-[300px] lg:w-[500px] lg:h-[350px]" />
+        <div className="relative z-10 grid md:grid-cols-12 w-full pt-24">
+            <div className="col-span-6 pb-7 md:pb-0 md:pl-4 max-w-[400px] md:max-w-[500px] m-auto">
+                <h1 className="text-4xl lg:text-5xl font-bold leading-snug lg:leading-normal">Enjoy your <span className="text-[#FF902A]">coffee</span> before your activity</h1>
                 <p>Boost your productivity and build your mood with a glass of coffee in the morning </p>
-                <div className="space-x-3">
-                  <button className="bg-[#2F2105] text-white rounded-3xl py-2 w-32">Order Now</button>
+                <div className="flex items-center space-x-3">
+                  <button className="flex items-center justify-center bg-[#2F2105] text-white rounded-full py-3 w-44">Order Now
+                  <AiOutlineShoppingCart className=" w-5 h-5 rounded-full ml-2 p-1 bg-[#FF902A]" size={15} />
+                  </button>
                   <button className="bg-transparent text-[#FF902A] border-none py-2 w-32">More Menu</button>
                 </div>
             </div>
@@ -22,7 +25,7 @@ const Hero = () => {
                 <Image src={coffee} alt="coffee" priority />
             </div>
         </div>
-        <Image src={nuts2} alt="nuts" priority className="" />
+        <Image src={nuts2} alt="nuts" priority className="relative left-0 -top-4 md:-top-5 w-[250px] md:w-[400px] h-[300px] lg:w-[500px] lg:h-[350px]" />
     </section>
   )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,90 +1,100 @@
 'use client'
 
-import Image from "next/image";
+import Image from 'next/image';
 import cafe from '@/images/logo.svg';
-import { Dancing_Script } from "next/font/google";
-import {AiOutlineSearch, AiOutlineShoppingCart, AiOutlineClose} from "react-icons/ai";
-import {CgMenuHotdog} from "react-icons/cg";
-import { useState } from "react";
-import { motion } from "framer";
+import { Dancing_Script } from 'next/font/google';
+import { AiOutlineSearch, AiOutlineShoppingCart, AiOutlineClose } from 'react-icons/ai';
+import { CgMenuHotdog } from 'react-icons/cg';
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const dancingscript = Dancing_Script({
-    weights: ["700"],
-    subsets: ['latin'],
-  })
-
+  weights: ['700'],
+  subsets: ['latin'],
+});
 
 const Navbar = () => {
-  const [nav, setNav] = useState()
+  const [nav, setNav] = useState(false);
 
   const handleClick = () => {
-    return setNav(!nav)
-  }
+    setNav(!nav);
+  };
 
   return (
-    <section className="fixed z-50 w-full h-auto my-5 ">
-        <div className="max-w-[400px] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[1100px] mx-auto flex justify-between items-center">
-            <div className="flex items-center space-x-1">
-                <Image src={cafe} alt="cafe" priority className="" />
-                <h1 className={`text-3xl ${dancingscript.className}`}><span className="text-[#FF902A]">Cafe</span> Street</h1>
+    <section className="fixed z-50 w-full h-auto py-3">
+      <div className="max-w-[400px] sm:max-w-[600px] md:max-w-[800px] lg:max-w-[1100px] mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-1">
+          <Image src={cafe} alt="cafe" priority className="animate-bounce" />
+          <h1 className={`text-3xl md:text-4xl ${dancingscript.className}`}>
+            <span className="text-[#FF902A]">Cafe</span> Street
+          </h1>
+        </div>
+        <div className="hidden md:flex justify-between items-center w-[400px]">
+          <h2 className="text-lg">About Us</h2>
+          <h2 className="text-lg">Our Product</h2>
+          <h2 className="text-lg">Delivery</h2>
+        </div>
+        <div className="flex items-center space-x-4">
+          <form>
+            <div className="hidden sm:flex items-center">
+              <AiOutlineSearch className="absolute z-3 text-gray-500 ml-1" size={25} />
+              <input
+                className="w-full h-9 pl-8 bg-gray-200 rounded-xl text-gray-500"
+                type="text"
+                placeholder="Menu"
+              />
             </div>
-            <div className="hidden sm:flex justify-between items-center w-[300px]">
-                <h2>About Us</h2>
-                <h2>Our Product</h2>
-                <h2>Delivery</h2>
-            </div>            
-            <div className="flex items-center space-x-4">
-                <form>
-                    <div className="hidden sm:flex items-center">
-                        <AiOutlineSearch className="absolute z-3 text-gray-500 ml-1" size={25} />
-                        <input
-                        className="w-full h-9 pl-8 bg-dark/60 rounded-xl text-gray-500"
-                        type="text"
-                        // value="Search"
-                        // onChange={(e) => setSearch(e.target.value)}
-                        placeholder= "Menu"
-                        />
-                    </div>
-                </form>
-                <AiOutlineShoppingCart size={25} />
-            </div>
-            <div className="sm:hidden">
-              {nav ? (
-                <div className="fixed top-0 right-0 h-screen w-1/2 bg-[#2F2105] text-white z-50">
-                <div className="flex mt-5 space-x-32 px-5">
+          </form>
+          <AiOutlineShoppingCart size={25} />
+        </div>
+        <div className="sm:hidden">
+          <AnimatePresence mode='wait'>
+            {nav ? (
+              <motion.div
+                key="menu"
+                initial={{ opacity: 0, x: '100%' }}
+                animate={{ opacity: 1, x: 0, transition: { duration: 0.5 } }}
+                exit={{ opacity: 0, x: '100%', transition: { duration: 1.5 } }}
+                className="fixed top-0 right-0 h-screen w-1/2 bg-[#2F2105] text-white z-50"
+              >
+                <div className="flex mt-4 space-x-28 px-5">
                   <AiOutlineShoppingCart size={25} />
                   <AiOutlineClose onClick={handleClick} size={25} />
                 </div>
-                  <div className="flex flex-col w-full h-full space-y-6 mt-10 text-3xl pl-3">
-                    <h2>About Us</h2>
-                    <h2>Our Product</h2>
-                    <h2>Delivery</h2>
-                  </div>
+                <div className="flex flex-col w-full h-full space-y-6 mt-10 text-3xl pl-5">
+                  <motion.h2
+                    initial={{ opacity: 0, x: '100%' }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 0.8 } }}
+                    exit={{x: '100%', transition: { duration: 0.5 } }}
+                  >About Us</motion.h2>
+                  <motion.h2
+                    initial={{ opacity: 0, x: '100%' }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 1.1 } }}
+                    exit={{x: '100%', transition: { duration: 0.8 } }}
+                  >Our Product</motion.h2>
+                  <motion.h2
+                    initial={{ opacity: 0, x: '100%' }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 1.3 } }}
+                    exit={{ x: '100%', transition: { duration: 1 } }}
+                  >Delivery</motion.h2>
                 </div>
-              ) : (
-                <div onClick={handleClick}>
+              </motion.div>
+            ) : (
+              <motion.div
+                key="icon"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, transition: { duration: 1.3 } }}
+                exit={{ opacity: 0, transition: { duration: 0.5 } }}
+                onClick={handleClick}
+              >
                 <CgMenuHotdog size={25} />
-              </div>
-              )}
-            </div>
-            
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Navbar;
-
-// <div onClick={handleClick}>
-//               <CgMenuHotdog size={25} />
-//             </div>
-//             {nav && (
-//               <div className="bg-gray-300">
-//                 <AiOutlineClose size={25} />
-//                 <div className="flex flex-col justify-between items-center w-[300px]">
-//                   <h2>About Us</h2>
-//                   <h2>Our Product</h2>
-//                   <h2>Delivery</h2>
-//                 </div>
-//               </div>
-//             )}
